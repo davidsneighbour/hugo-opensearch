8a3c9270054df15d65dd41477d7437641391d304
Patrick Kollitsch <patrick@davids-neighbour.com>
2022-04-21 17:16:23 +0700

    chore: fix setup files

--- .versionrc.js
+++ .versionrc.js
@@ -3,7 +3,7 @@ const defaultStandardVersion = require("@davidsneighbour/standard-version-config
 const localStandardVersion = {
   bumpFiles: [
     ...defaultStandardVersion.bumpFiles,
-    { filename: "data/dnb/opensearch/version.json", type: "json" },
+    { filename: "data/dnb/opensearch/build.json", type: "json" },
   ],
 };
 
